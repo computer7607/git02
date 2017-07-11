e7ce6efff53cab2664c196d01e3b43fc42874521
wqf <computer7607@163.com>
2017-07-11 16:25:31 +0800

    游戏编号修改

--- 删除游戏记录.sql
+++ 删除游戏记录.sql
@@ -1,48 +1,48 @@
-select * from GM_AWARD_FILE where GAME_CODE = '0312' ;
-select * from GM_GAME_BUSI_STATUS where GAME_CODE = '0312' ;
-select * from GM_PRIZE_LEVEL_FILE t where game_code = '0312' ;
+select * from GM_AWARD_FILE where GAME_CODE = '9014' ;
+select * from GM_GAME_BUSI_STATUS where GAME_CODE = '9014' ;
+select * from GM_PRIZE_LEVEL_FILE t where game_code = '9014' ;
 --入库明细汇总 
-select * from WM_TRADE_IN_SUMMARY t where t.game_code='0312' ;
+select * from WM_TRADE_IN_SUMMARY t where t.game_code='9014' ;
 --游戏印刷批次 
-select * from GM_GAME_PRINT_BATCH t where t.game_code='0312';
+select * from GM_GAME_PRINT_BATCH t where t.game_code='9014';
 --库存
-select * from WM_INVENTORY t where t.game_code='0312';
+select * from WM_INVENTORY t where t.game_code='9014';
 --库存流水 
-select count(*) from WM_INVENTORY_FLOW t where t.game_code='0312';
+select count(*) from WM_INVENTORY_FLOW t where t.game_code='9014';
 --容器历史跟踪;
-select count(*)from WM_CONTAINER_HIS t where t.game_code='0312';
+select count(*)from WM_CONTAINER_HIS t where t.game_code='9014';
 --入库容器明细 
-select count(*) from WM_TRADE_IN_CONTAINER t where t.game_code='0312';
-select  * from WM_TRADE_IN_CONTAINER t where t.game_code='0312';
+select count(*) from WM_TRADE_IN_CONTAINER t where t.game_code='9014';
+select  * from WM_TRADE_IN_CONTAINER t where t.game_code='9014';
  --容器
-select count(*) from WM_CONTAINER t where t.game_code='0312';
+select count(*) from WM_CONTAINER t where t.game_code='9014';
 --运输文件明细
-select * from PM_SEAL_DOC t where t.game_code='0312';
+select * from PM_SEAL_DOC t where t.game_code='9014';
 --工作单
-select * from PM_PURCHASE_ORDER  t WHERE t.game_code='0312';
+select * from PM_PURCHASE_ORDER  t WHERE t.game_code='9014';
 --任务单
-select * from PM_PRODUCE_ORDER t WHERE t.game_code='0312';
+select * from PM_PRODUCE_ORDER t WHERE t.game_code='9014';
 --质检单
-select * from PM_QUALITY_DOC t where t.game_code='0312';
+select * from PM_QUALITY_DOC t where t.game_code='9014';
 --封存单
-select * from PM_SEAL_DOC t where t.game_code='0312';
---delete from PM_SEAL_DOC t where t.game_code='0312';
+select * from PM_SEAL_DOC t where t.game_code='9014';
+--delete from PM_SEAL_DOC t where t.game_code='9014';
 
 --采购订单
-select * from PM_PURCHASE_ORDER t where t.game_code='0312';
+select * from PM_PURCHASE_ORDER t where t.game_code='9014';
 --游戏
-select * from GM_GAME t where t.game_code='0312';
+select * from GM_GAME t where t.game_code='9014';
 --国家仓库到货入库明细
-select * from WM_TRADE_IN_DETAIL t where t.game_code='0312';
+select * from WM_TRADE_IN_DETAIL t where t.game_code='9014';
 
 --游戏
-select * from GM_GAME t where t.game_code='0312';
-select * from gm_import_file_his t where t.game_code='0312';
-select * from gm_transport_file t where t.game_code='0312';
+select * from GM_GAME t where t.game_code='9014';
+select * from gm_import_file_his t where t.game_code='9014';
+select * from gm_transport_file t where t.game_code='9014';
 
-select * from gm_transport_file_detail t where t.game_code='0312';
-select count(unique(award_seq_no)) from gm_transport_file_detail t where t.game_code='0312';
-select * from gm_game_prize_level where game_code = '0312'
+select * from gm_transport_file_detail t where t.game_code='9014';
+select count(unique(award_seq_no)) from gm_transport_file_detail t where t.game_code='9014';
+select * from gm_game_prize_level where game_code = '9014'
 
 
 ---------------------------------------------------------------------------------------------------------------------------
@@ -66,12 +66,12 @@ delete from gm_transport_file_detail t where t.game_code='9014' and t.transport_
 --1.查询是否为1万条数据
 select count(*)
   from gm_game_award_detail t
- where t.supgamecode = '350312'
+ where t.supgamecode = '359014'
  and t.gmawardfileid = '300379215812984243';
  
 --2.删除1万条数据
 delete from gm_game_award_detail t
- where t.supgamecode = '350312'
+ where t.supgamecode = '359014'
  and t.gmawardfileid = '300379215812984243';
 --3.确认执行结果是否影响了1万条数据
 --4.提交
@@ -79,7 +79,7 @@ commit;
 --5.再次确认是否已经删除，为0则为已经删除
 select count(*)
   from gm_game_award_detail t
- where t.supgamecode = '350312'
+ where t.supgamecode = '359014'
  and t.gmawardfileid = '300379215812984243';
 
 --35游戏导入成功后删除
